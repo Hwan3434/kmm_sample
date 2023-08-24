729b337e5d528d064d734aaf0afd708a1eda2db0
hwan3434 <cookiegon3434@gmail.com>
2023-08-24 21:21:16 +0900

    create init !

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMMSample"
+include(":androidApp")
+include(":shared")
